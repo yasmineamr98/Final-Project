a47b5cb8f2bf5ea92ee7149f5d992c9f1fab107e
yasmineamr98 <yasmine_amr2008@hotmail.com>
2024-09-11 14:33:55 +0300

    file structure
    Co-authored-by: Hussien Shokry <hessin2010king@users.noreply.github.com>

---
+++ Graduation-Project/Events-App/src/app/user/user-homepage/user-homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-homepage.component.html',
+  styleUrl: './user-homepage.component.css'
+})
+export class UserHomepageComponent {
+
+}
---
+++ Graduation-Project/Events-App/src/app/user/user-notification/user-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-notification.component.html',
+  styleUrl: './user-notification.component.css'
+})
+export class UserNotificationComponent {
+
+}
