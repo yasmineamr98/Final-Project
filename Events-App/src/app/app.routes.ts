d9bf1e2438fe301aadf181dc7025b2dd4350ebe0
hussien shokry <hessinshokry3@gmail.com>
2024-09-24 08:55:00 +0300

    solving routing issue

--- Events-App/src/app/app.routes.ts
+++ Events-App/src/app/app.routes.ts
@@ -3,25 +3,22 @@ import { UserLoginComponent } from '../app/user/user-login/user-login.component'
 import { UserRegisterComponent } from '../app/user/user-register/user-register.component';
 import { UserHomepageComponent } from './user/user-homepage/user-homepage.component';
 import { UserNotificationComponent } from './user/user-notification/user-notification.component';
-import { UserSettingsComponent } from './user/user-settings/user-settings.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
 import { EventDetailsComponent } from './pages/event-details/event-details.component';
 import { EventsComponent } from './pages/events/events.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HomeComponent } from './pages/home/home.component';
-import { UserProfileComponent } from './user/user-profile/user-profile.component';
 
 export const routes: Routes = [
-  { path: 'user-login', component: UserLoginComponent },
+  { path: 'user-login', component: UserLoginComponent }, // Login route
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'user-register', component: UserRegisterComponent },
-
   { path: 'user-homepage', component: UserHomepageComponent },
   { path: 'user-notification', component: UserNotificationComponent },
-  { path: 'user-profile', component: UserProfileComponent },
-  { path: 'user-settings', component: UserRegisterComponent },
+  { path: 'user-profile/:id', component: UserProfileComponent }, // User profile route
   { path: 'event-details', component: EventDetailsComponent },
   { path: 'events', component: EventsComponent },
   { path: 'footer', component: FooterComponent },
