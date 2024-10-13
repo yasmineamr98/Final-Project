90ec9f16cee17867f850712088451e21a2be0716
OmarAlhlawany <omarhlw1162@gmail.com>
2024-10-13 19:37:48 +0300

    Impelemnt Subscription and fix footer routing

--- Events-App/src/app/pages/footer/footer.component.ts
+++ Events-App/src/app/pages/footer/footer.component.ts
@@ -2,13 +2,47 @@ import { Component } from '@angular/core';
 import { Route, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { SubscribeService } from '../../services/subscribe.service';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink, TranslateModule],
+  imports: [CommonModule, RouterLink, TranslateModule, ReactiveFormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-  router: any;
+  subscribeForm: FormGroup;
+  subscriptionSuccess = false;
+  subscriptionError = false;
+
+  constructor(
+    private subscribeService: SubscribeService,
+    private formBuilder: FormBuilder
+  ) {
+    this.subscribeForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    if (this.subscribeForm.valid) {
+      const email = this.subscribeForm.get('email')?.value;
+
+      this.subscribeService.subscribeUser(email).subscribe(
+        (response) => {
+          this.subscriptionSuccess = true;
+          this.subscriptionError = false;
+          this.subscribeForm.reset();
+        },
+        (error) => {
+          this.subscriptionError = true;
+          this.subscriptionSuccess = false;
+          console.error('Subscription failed', error);
+        }
+      );
+    }
+  }
 }
